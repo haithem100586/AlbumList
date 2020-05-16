3fdcda93fbc7048ef1445dec897f2917768bcf88
Haithem <rsi.helloumi@gmail.com>
2020-05-17 01:29:42 +0200

    add espresso ui test methods

--- app/src/androidTest/java/com/android/albumlist/AppTest.kt
+++ app/src/androidTest/java/com/android/albumlist/AppTest.kt
@@ -1,5 +1,7 @@
 package com.android.albumlist
 
+import com.android.albumlist.framework.di.DaggerTestAppComponent
+
 class AppTest: App() {
     override fun initDagger() {
         DaggerTestAppComponent.builder().application(this).build().inject(this)
